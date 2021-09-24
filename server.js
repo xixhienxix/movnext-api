51fa7d7e068597d2862650547d118fcf33540f05
xixhienxix <carlos_mf_90@hotmail.com>
2021-09-24 17:27:39 -0500

    push to evennode

--- server.js
+++ server.js
@@ -1,63 +1,133 @@
-const app = require("./app");
-const debug = require("debug")("node-angular");
-const http = require("http");
-var https = require('https');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const port = 4000
 
 
-const normalizePort = val => {
-  var port = parseInt(val, 10);
+const huespedController = require('./controllers/huesped')
+const foliocontroller = require('./controllers/folio')
+const estatusController = require('./controllers/estatus')
+const habitacioncontroller = require('./controllers/habitacion')
+const disponibilidadController = require('./controllers/disponibilidad')
+const bloqueoController = require('./controllers/bloqueo')
+const versionController = require('./controllers/version')
+const origenController = require('./controllers/origen')
+const historicoController = require ('./controllers/historico')
+const adicionaController = require ('./controllers/adicional')
+const authController = require ('./controllers/auth')
 
-  if (isNaN(port)
-  ) {
-    // named pipe
-    return val;
-  }
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+const cors = require('cors');
+const app = express();
 
-  return false;
-};
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
-const onError = error => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('debug', true);//Muestra el Query en Consola
+mongoose.connect(
+  "mongodb://xixzeroxix:34nj6efH@cluster0-shard-00-00.kjzuz.mongodb.net:27017,cluster0-shard-00-01.kjzuz.mongodb.net:27017,cluster0-shard-00-02.kjzuz.mongodb.net:27017/MovNext?ssl=true&replicaSet=atlas-lzt57i-shard-0&authSource=admin&retryWrites=true&w=majority",
+  { useNewUrlParser: true })
+.then(()=>{
+  console.log("Connexion a BD Correcta 123")
+}).catch(error => handleError(error));
 
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-  debug("Listening on " + bind);
-};
 
-const port = normalizePort(process.env.PORT || "4000");
-app.set("port", port);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-  var result = 'App is running'
-  response.send(result);
-}).listen(app.get('port'), function() {
-  console.log('App is running, server is listening on port ', app.get('port'));
+//POST
+ app.post("/api/reportes/huesped",huespedController.postHuesped);
+
+ app.post("/api/reportes/habitacion/estatus/:id",habitacioncontroller.postEstatusHabitacion);
+
+ app.post("/api/reportes/bloqueos/post",bloqueoController.postBloqueos);
+
+ app.post('/api/actualiza/bloqueos',bloqueoController.actualizaBloqueos);
+
+ app.post("/api/libera/bloqueos",bloqueoController.liberaBloqueos);
+
+ app.post("/api/reportes/actualiza/huesped",huespedController.actualizaHuesped)
+
+ app.post("/api/reportes/historico",historicoController.postHistorico);
+
+ app.post("/api/actualiza/estatus",estatusController.updateEstatus);
+
+ app.post("/api/reportes/actualiza/estatus/huesped",huespedController.actualizaEstatusHuesped)
+
+ app.post("/api/auth/login",authController.login)
+
+ app.post("/api/auth/registro",authController.registro)
+//GET
+
+app.get('/api/version',versionController.getVersion)
+
+app.get('/api/reportes/origen',origenController.getOrigen)
+
+app.get('/api/reportes/huesped',huespedController.getHuesped)
+
+app.get('/api/reportes/historico',huespedController.getHuespedHistorico)
+
+app.get('/api/reportes/huesped/:id',huespedController.getHuespedbyId);
+
+app.get('/api/reportes/folios/:letra', foliocontroller.getFoliosbyLetra);
+
+app.get('/api/reportes/folios', foliocontroller.getFolios);
+
+//Habitaciones
+
+app.get('/api/reportes/habitaciones',habitacioncontroller.getHabitacion)
+
+app.get('/api/reportes/habitaciones/:codigo',habitacioncontroller.getHabitacionbyId);
+
+app.get('/api/reportes/habitacion/:numero',habitacioncontroller.getHabitacionbyNumber);
+
+app.get('/api/info/habitaciones',habitacioncontroller.getInfoHabitaciones);
+
+app.get('/api/reportes/tipo',habitacioncontroller.getCodigoHabitacion);
+
+//Disponibilidad
+
+app.get('/api/huespedes/disponibilidad',disponibilidadController.getDisponibilidadXFecha)
+
+app.get('/api/huespedes/disponibilidad/todos',disponibilidadController.getDisponibilidadTodos)
+
+//Estatus
+
+app.get('/api/reportes/estatus',estatusController.getEstatus)
+
+app.get('/api/reportes/estatus/:id',estatusController.getEstatusbyId);
+
+//Bloqueos
+
+app.get('/api/reportes/bloqueos',bloqueoController.getBloqueos)
+
+app.get('/api/get/bloqueos/:id',bloqueoController.getBloqueosbyId);
+
+//Adicional
+
+app.get('/api/adicionales',adicionaController.getAdicional)
+
+
+
+//DELETE
+
+app.delete("/api/reportes/borrar-bloqueo/:id", bloqueoController.deleteBloqueo)
+
+//PUT
+
+
+
+
+app.use(function(error, req, res, next){
+  res.json(error);
 });
 
-// const server = http.createServer(app);
-// server.on("error", onError);
-// server.on("listening", onListening);
-// server.listen(port);
+
+
+
+
+module.exports = app;
